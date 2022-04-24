76181494ca0297de0faf86692e4cc9d7bd126808
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-24 16:32:17 -0500

    2. Adding HTTP METHODS: GET ALL

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ const port = 3000
 app.listen(port, () => {
     console.log(`Example app listening on port: ${port}`)
 })
+
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+    console.log(`API explorers GET ALL requests ${new Date()}`)
+    const explorer1 = { id: 1, name: "Marcos1"}
+    const explorer2 = { id: 2, name: "Marcos2"}
+    const explorer3 = { id: 3, name: "Marcos3"}
+    const explorer4 = { id: 4, name: "Marcos4"}
+    const explorers = [ explorer1, explorer2, explorer3, explorer4 ]
+    res.status(200).json(explorers)
+})
